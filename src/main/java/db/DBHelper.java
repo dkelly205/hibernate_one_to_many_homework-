901c16d0506ce809de7066b8e90e55a5a947ac73
daniel kelly <danieljkelly91@gmail.com>
2018-03-12 23:53:09 +0000

    added getOrders and getAll methods in DBHelper

--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -1,8 +1,12 @@
 package db;
 
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+
+import java.util.List;
 
 public class DBHelper {
 
@@ -23,9 +27,46 @@ public class DBHelper {
         }
     }
 
+    public static <T> List<T> getAll(String className) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<T> results = null;
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from " + className;
+            results = session.createQuery(hql).list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    public static List<Order> getOrders(int id){
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<Order> orders = null;
+
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from Order WHERE customer_id = :id";  //:id is a placeholder, prevent sql injection
+            Query query = session.createQuery(hql);
+            query.setInteger("id", id);//the "id" is linked to the placeholder :id above, the other id is the value that you want to set, it is an argument in the method getEmployees above
+            orders = query.list();
+            transaction.commit();
+
+        }catch (HibernateException e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return orders;
+    }
+
     public static void delete(Object object){
         session = HibernateUtil.getSessionFactory().openSession();
-
         try{
             transaction = session.beginTransaction();
             session.delete(object);
